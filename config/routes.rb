82d9d8059cb40a5fefdf2933eec0d30e157d72ee
arcticfoxnz <arcticfoxnz@gmail.com>
2013-09-30 22:56:01 +1300

    user validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Manticore::Application.routes.draw do
   resources :users
+  resources :addresses
+  resources :cities
+  resources :jurisdictions
+  resources :countries
 
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
